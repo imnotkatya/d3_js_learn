4257ee0e59cd87b406de2f752b0f34bbd3262c2e
imnotkatya <k.bydanova@inbox.ru>
2025-10-28 07:53:46 +0000

    full data

---
+++ data.js
@@ -0,0 +1,186 @@
+
+  const width = 600;
+  const height = 800;
+  const marginTop = 30;
+  const marginRight = 30;
+  const marginBottom = 30;
+  const marginLeft = 120; 
+
+  const dataset = await FileAttachment("followup-2@1.csv").csv(); 
+  const toParseColor = await FileAttachment("color.csv").csv(); 
+
+  const colors = toParseColor.map(d => ({
+    key: d.key,
+    color: d.color,
+    stroke_dash: +d.stroke_dash, 
+    y_modify: +d.y_modify,  
+    stroke: d.stroke,
+    strokeWidth: +d['stroke-width']
+  }));
+
+  const parsedDataset = dataset.map(d => ({
+    name: d.name,
+    ro: d.ro,
+    start: +d.start, 
+    end: +d.end,  
+    type: d.type,
+    r: +d.r,
+    face: +d.face,
+    direction: +d.direction,
+  }));
+
+  const datagrouped = d3.group(parsedDataset, (d) => d.name);
+  const sortedData = Array.from(datagrouped)
+    .sort((a, b) => {
+      const aAvg = d3.max(a[1].map(d => d.end));
+      const bAvg = d3.max(b[1].map(d => d.end));
+      return aAvg - bAvg;
+    })
+    .flatMap(([id, types]) => types);
+
+  const svg = d3.create("svg")
+    .attr("width", width)
+    .attr('height', height);
+
+  const uniqueNames = [...new Set(sortedData.map(d => d.name))];
+  const y = d3.scaleBand()
+    .domain(uniqueNames) 
+    .paddingInner(0.2)
+    .range([height - marginBottom, marginTop]);
+
+  
+  svg.selectAll("ro")
+    .data(uniqueNames)
+    .enter()
+    .append("text")
+    .attr("x", marginLeft - 70)
+    .attr("y", d => y(d) + y.bandwidth() / 2)
+    .attr("dy", "4px")
+    .text(d => {
+      const patient = sortedData.find(p => p.name === d);
+      return patient.ro;
+    })
+    .style("font-size", "12px");
+
+  const x = d3.scaleLinear()
+    .domain([0, 50])
+    .range([marginLeft, width - marginRight]);
+  
+  const color = d3.scaleOrdinal()
+    .domain(colors.map(c => c.key))
+    .range(colors.map(c => c.color));
+
+  const stroke_color = d3.scaleOrdinal()
+    .domain(colors.map(c => c.key))
+    .range(colors.map(c => c.stroke));
+
+  const stroke_dash = d3.scaleOrdinal()
+    .domain(colors.map(c => c.key))
+    .range(colors.map(c => c.stroke_dash));
+
+  const stroke_width = d3.scaleOrdinal()
+    .domain(colors.map(c => c.key))
+    .range(colors.map(c => c.strokeWidth));
+  
+  const y_modified = d3.scaleOrdinal()
+    .domain(colors.map(c => c.key))
+    .range(colors.map(c => c.y_modify));
+
+ 
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x).tickValues([0, 6, 12, 18, 24, 30, 36, 42, 48]));
+
+  svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y));
+
+  
+  const elements = svg.selectAll("data")
+    .data(sortedData)
+    .enter()
+    .append("g")
+    .attr("opacity", d => d.start >= 0 ? 1 : 0);
+
+
+elements.filter(d => d.type === "vemu" || d.type === "followUp" || d.type === "chemo" || d.type === "vemu_r")
+    .append("rect")
+    .attr("fill", d => color(d.type))
+    .attr("stroke", d => stroke_color(d.type))
+    .attr("stroke-dasharray", d => stroke_dash(d.type))
+    .attr("y", d => y(d.name) + y_modified(d.type)) 
+    .attr("x", d => x(d.start))
+    .attr("height", y.bandwidth()) 
+    .attr("width", d => Math.max(0, x(d.end) - x(d.start)));
+
+
+  elements.filter(d => d.type === "circle")
+    .append("circle")
+    .attr("cx", d => x(d.start)) 
+    .attr("cy", d => y(d.name) + y.bandwidth() / 2 + y_modified(d.type)) 
+    .attr("r", d => d.r)
+    .attr("fill", d => color(d.type))
+    .attr("stroke", d => stroke_color(d.type))
+    .attr("stroke-width", d => stroke_width(d.type));
+
+ 
+  elements.filter(d => d.type === "cube")
+    .append("rect")
+    .attr("x", d => x(d.start) - (d.face / 2)) 
+    .attr("y", d => y(d.name) + y.bandwidth() / 2 - (d.face / 2) + y_modified(d.type)) 
+    .attr("width", d => d.face)
+    .attr("height", d => d.face)
+    .attr("fill", d => color(d.type))
+    .attr("stroke", d => stroke_color(d.type))
+    .attr("stroke-width", d => stroke_width(d.type));
+
+  
+
+  elements.filter(d => d.type === "myeloid"||d.type === "mds"||d.type === "relapse"||d.type === "cfdna"||d.type === "arrow")
+    .append("polygon")
+    .attr("points", d => {
+      const size = d.face; 
+      const xPos = x(d.start);
+      const yPos = y(d.name) + y.bandwidth() / 2 + y_modified(d.type);
+      
+      switch(d.type) {
+        case "myeloid":
+          return `${xPos},${yPos + size} ${xPos - size},${yPos - size} ${xPos + size},${yPos - size}`;
+        case "cfdna":
+          return `${xPos},${yPos + size} ${xPos - size},${yPos - size} ${xPos + size},${yPos - size}`;
+        case "relapse":
+           return `${xPos},${yPos + size} ${xPos - size},${yPos - size} ${xPos + size},${yPos - size}`;
+        case "mds":
+          return `${xPos},${yPos - size} ${xPos + size},${yPos} ${xPos},${yPos + size} ${xPos - size},${yPos}`;
+        case "arrow":
+          return `${xPos - size},${yPos - size/2} ${xPos + size},${yPos - size/2} ${xPos + size},${yPos - size} ${xPos + size * 2},${yPos} ${xPos + size},${yPos + size} ${xPos + size},${yPos + size/2} ${xPos - size},${yPos + size/2}`;
+        default:
+          return "";
+      }
+    })
+    .attr("fill", d => color(d.type))
+    .attr("stroke", d => stroke_color(d.type))
+    .attr("stroke-width", d => stroke_width(d.type));
+
+
+  svg.selectAll("legend_rect")
+    .data(colors.map(c => c.key))
+    .enter()
+    .append("rect")
+    .attr("x", width - 100)
+    .attr("y", (d, i) => height/2 + 90 + i * 25) 
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("stroke", "black")
+    .style("fill", d => color(d));
+
+  svg.selectAll("legend_labels")
+    .data(colors.map(c => c.key))
+    .enter()
+    .append("text")
+    .attr("x", width - 170)
+    .attr("y", (d, i) => height - 295 + i * 25) 
+    .style("fill", 'black')
+    .text(d => d);
+
+  return svg.node();
